62390d7fe5ec0c308128b0c09bd27b7f91f671c3
joo <vividjtj@gmail.com>
2022-03-27 23:49:33 +0900

    1st typeorm

---
+++ src/data-source.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Photo } from "./entity/Photo";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "",
+  password: "",
+  database: "typeorm",
+  synchronize: true,
+  logging: false,
+  entities: [User, Photo],
+  migrations: [],
+  subscribers: [],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    // here you can start to work with your database
+    console.log(`AppDataSource ini!`);
+  })
+  .catch((error) => console.log(error));
---
+++ src/entity/Photo.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  name: string;
+  @Column("text")
+  description: string;
+  @Column()
+  filename: string;
+  @Column()
+  views: number;
+  @Column()
+  isPublished: boolean;
+}
